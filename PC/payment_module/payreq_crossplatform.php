ede62b6bc294167ae14ef895f13335d4e2440787
ojw <ojoonwoo@gmail.com>
2017-08-17 12:47:25 +0900

    no message

--- PC/payment_module/payreq_crossplatform.php
+++ PC/payment_module/payreq_crossplatform.php
@@ -116,8 +116,8 @@ session_start();
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<!-- <meta http-equiv="Content-Type" content="text/html; charset=euc-kr"> -->
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<!-- <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> -->
 <title>LG 유플러스 eCredit서비스 결제테스트</title>
 <script language="javascript" src="http://xpay.uplus.co.kr/xpay/js/xpay_crossplatform.js" type="text/javascript"></script>
 <script type="text/javascript">
