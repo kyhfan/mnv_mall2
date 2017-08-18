c13093899cd0ac40a4325fda4538150fc463cb8d
ojw <ojoonwoo@gmail.com>
2017-08-18 14:50:24 +0900

    no message

--- PC/pay/Cancel1.php
+++ PC/pay/Cancel1.php
@@ -1,44 +1,36 @@
-<?php
-    /*
-     * [결제취소 요청 페이지]
-     *
-     * LG유플러스으로 부터 내려받은 거래번호(LGD_TID)를 가지고 취소 요청을 합니다.(파라미터 전달시 POST를 사용하세요)
-     * (승인시 LG유플러스으로 부터 내려받은 PAYKEY와 혼동하지 마세요.)
-     */
-    $CST_PLATFORM               = $_POST["CST_PLATFORM"];       //LG유플러스 결제 서비스 선택(test:테스트, service:서비스)
-    $CST_MID                    = $_POST["CST_MID"];            //상점아이디(LG유플러스으로 부터 발급받으신 상점아이디를 입력하세요)
-                                                                         //테스트 아이디는 't'를 반드시 제외하고 입력하세요.
-    $LGD_MID                    = (("test" == $CST_PLATFORM)?"t":"").$CST_MID;  //상점아이디(자동생성)
-    $LGD_TID                	= $_POST["LGD_TID"];			 //LG유플러스으로 부터 내려받은 거래번호(LGD_TID)
-
- 	$configPath 				= "C:/lgdacom"; 						 //LG유플러스에서 제공한 환경파일("/conf/lgdacom.conf") 위치 지정.
-
-    require_once("./lgdacom/XPayClient.php");
-    $xpay = &new XPayClient($configPath, $CST_PLATFORM);
-    $xpay->Init_TX($LGD_MID);
-
-    $xpay->Set("LGD_TXNAME", "Cancel");
-    $xpay->Set("LGD_TID", $LGD_TID);
-
-    /*
-     * 1. 결제취소 요청 결과처리
-     *
-     * 취소결과 리턴 파라미터는 연동메뉴얼을 참고하시기 바랍니다.
-     */
-    if ($xpay->TX()) {
-        //1)결제취소결과 화면처리(성공,실패 결과 처리를 하시기 바랍니다.)
-        // echo "결제 취소요청이 완료되었습니다.  <br>";
-        // echo "TX Response_code = " . $xpay->Response_Code() . "<br>";
-        // echo "TX Response_msg = " . $xpay->Response_Msg() . "<p>";
-
-
-        // DB 처리
-        echo json_encode($xpay->Response_Code(),$xpay->Response_Msg());
-    }else {
-        //2)API 요청 실패 화면처리
-        // echo "결제 취소요청이 실패하였습니다.  <br>";
-        // echo "TX Response_code = " . $xpay->Response_Code() . "<br>";
-        // echo "TX Response_msg = " . $xpay->Response_Msg() . "<p>";
-        echo json_encode($xpay->Response_Code(),$xpay->Response_Msg());
-    }
+<?
+    include_once $_SERVER['DOCUMENT_ROOT']."/config.php";
+    include_once $_mnv_PC_dir."header.php";
 ?>
+<html>
+<head>
+    <title>LG유플러스 전자결제 결제취소 샘플 페이지</title>
+</head>
+<body>
+    <form method="post" id="LGD_PAYINFO" action="Cancel.php">
+    <div>
+        <!-- 주문 취소시 해당 주문에 대한 상품 정보, 결제 정보 SELECT -->
+        <table>
+            <tr>
+                <td>상점아이디(t를 제외한 아이디) </td>
+                <td><input type="text" name="CST_MID" id="CST_MID" value="miniver"/></td>
+            </tr>
+            <tr>
+                <td>서비스,테스트 </td>
+                <td><input type="text" name="CST_PLATFORM" id="CST_PLATFORM" value="test"/></td>
+            </tr>
+            <tr>
+                <td>LG유플러스 거래번호 </td>
+                <td><input type="text" name="LGD_TID" id="LGD_TID" value=""/></td>
+            </tr>
+
+            <tr>
+                <td>
+                <input type="button" id="pay_cancel" value="결제 취소"/><br/>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
