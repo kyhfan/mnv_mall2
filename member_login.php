692137bada4c3799c77bc7069dff89e06a3bf0af
kyhfan <kyhfan@gmail.com>
2017-09-26 20:51:09 +0900

    페북 로그인 개발 중

--- member_login.php
+++ member_login.php
@@ -104,7 +104,7 @@
 				<a href="javascript:loginWithKakao('<?=$ref_url?>')" class="kt">
 					<span class="blind">카카오계정 로그인</span>
 				</a>
-				<a href="javascript:void(0)" class="fb">
+				<a href="javascript:void(0)" class="fb"  id="loginBtn">
 					<span class="blind">페이스북 로그인</span>
 				</a>
 				<a href="<?=$apiURL?>" class="nv">
@@ -160,6 +160,70 @@
 		});
 
 
-	</script>
+		function getUserData() {
+			/* FB.api('/me', function(response) {
+				document.getElementById('response').innerHTML = 'Hello ' + response.name;
+				console.log(response);
+			}); */
+			FB.api('/me', {fields: 'name,email,gender,birthday'}, function(response) {
+				console.log(JSON.stringify(response));
+				$("#name").text("이름 : "+response.name);
+				$("#email").text("이메일 : "+response.email);
+				$("#gender").text("성별 : "+response.gender);
+				$("#birthday").text("생년월일 : "+response.birthday);
+				$("#id").text("아이디 : "+response.id);
+			});
+		}
+		  
+		window.fbAsyncInit = function() {
+			//SDK loaded, initialize it
+			FB.init({
+				appId      : '1467962459907448',
+				cookie     : true,  // enable cookies to allow the server to access
+						// the session
+				xfbml      : true,  // parse social plugins on this page
+				version    : 'v2.8' // use graph api version 2.8
+			});
+		  
+			//check user session and refresh it
+			FB.getLoginStatus(function(response) {
+				if (response.status === 'connected') {
+					//user is authorized
+					//document.getElementById('loginBtn').style.display = 'none';
+					getUserData();
+				} else {
+					//user is not authorized
+				}
+			});
+		};
+		  
+		//load the JavaScript SDK
+		(function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) {return;}
+			js = d.createElement(s); js.id = id;
+			js.src = "//connect.facebook.com/ko_KR/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+		  
+		//add event listener to login button
+		document.getElementById('loginBtn').addEventListener('click', function() {
+			//do the login
+			FB.login(function(response) {
+				if (response.authResponse) {
+					access_token = response.authResponse.accessToken; //get access token
+					user_id = response.authResponse.userID; //get FB UID
+					console.log('access_token = '+access_token);
+					console.log('user_id = '+user_id);
+					$("#access_token").text("접근 토큰 : "+access_token);
+					$("#user_id").text("FB UID : "+user_id);
+					//user just authorized your app
+					//document.getElementById('loginBtn').style.display = 'none';
+					getUserData();
+				}
+			}, {scope: 'email,public_profile,user_birthday',
+				return_scopes: true});
+		}, false);
+</script>	
 </body>
 </html>
