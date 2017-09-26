ba4492424cbf79eb93a3ac1be92b09f6663f1017
kyhfan <kyhfan@gmail.com>
2017-09-26 18:35:06 +0900

    111

--- member_login.php
+++ member_login.php
@@ -14,26 +14,51 @@
 
 	// 
 	// 네이버 로그인 콜백
-	$client_id = "mebia0Wrk4RP6CBvbnwx";
-	$client_secret = "oc4ov9gvgn";
-	$code = $_GET["code"];
-	$state = $_GET["state"];
-	$redirectURI = urlencode("http://www.store-chon.com/dev/member_login.php?ref_url=$ref_url");
-	$url = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=".$client_id."&client_secret=".$client_secret."&redirect_uri=".$redirectURI."&code=".$code."&state=".$state;
-	$is_post = false;
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_POST, $is_post);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$headers = array();
-	$response = curl_exec ($ch);
-	$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	echo "status_code:".$status_code."";
-	curl_close ($ch);
-	if($status_code == 200) {
-		echo $response;
-	} else {
-		echo "Error 내용:".$response;
+	if ($_GET["code"])
+	{
+		$client_id = "mebia0Wrk4RP6CBvbnwx";
+		$client_secret = "oc4ov9gvgn";
+		$code = $_GET["code"];
+		$state = $_GET["state"];
+		$redirectURI = urlencode("http://www.store-chon.com/dev/member_login.php?ref_url=$ref_url");
+		$url = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=".$client_id."&client_secret=".$client_secret."&redirect_uri=".$redirectURI."&code=".$code."&state=".$state;
+		$is_post = false;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_POST, $is_post);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$headers = array();
+		$response = curl_exec ($ch);
+		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		echo "status_code:".$status_code."";
+		curl_close ($ch);
+		if($status_code == 200) {
+			//echo $response;
+			$json_data = json_decode($response, true);
+			$token = $json_data["access_token"];
+			$header = "Bearer ".$token; // Bearer 다음에 공백 추가
+			$url = "https://openapi.naver.com/v1/nid/me";
+			$is_post = false;
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_POST, $is_post);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$headers = array();
+			$headers[] = "Authorization: ".$header;
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			$response = curl_exec ($ch);
+			$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			echo "status_code:".$status_code."<br>";
+			curl_close ($ch);
+			if($status_code == 200) {
+				$json_data = json_decode($response, true);
+				print_r($json_data);  
+			} else {
+			  	echo "Error 내용:".$response;
+			}			
+		} else {
+			echo "Error 내용:".$response;
+		}
 	}	
 ?>						
 <body>
