3685230540c1e715ac8b9485610e1462c5e579e6
kyhfan <kyhfan@gmail.com>
2017-09-26 18:20:54 +0900

    1111

--- member_login.php
+++ member_login.php
@@ -8,7 +8,7 @@
 
 	// 네이버 로그인 접근토큰 요청 예제
 	$client_id = "mebia0Wrk4RP6CBvbnwx";
-	$redirectURI = urlencode("http://www.store-chon.com");
+	$redirectURI = urlencode("http://www.store-chon.com/dev/member_login.php?ref_url=$ref_url");
 	$state = "RAMDOM_STATE";
 	$apiURL = "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=".$client_id."&redirect_uri=".$redirectURI."&state=".$state;
 ?>						
