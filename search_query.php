4175cbc9bd58a42b60dd1bef3a04fba3cb47f926
kyhfan <kyhfan@gmail.com>
2017-11-13 19:02:00 +0900

    1111

--- search_query.php
+++ search_query.php
@@ -2,18 +2,18 @@
 	$word = $_REQUEST['word'];
 
 	$search_result_array = array();
-	foreach ($keyword_storage as $key => $value) {
-		if(strstr($value['goods_name'], $word) || strstr($value['goods_desc'], $word))
-		{
-			array_push($search_result_array, array('goods_code' => $value['goods_code'],
-													'goods_name' => $value['goods_name']) );
-		}
-	}
-	if(count($search_result_array) <= 0) {
-		$flag = "N";
-	} else {
-		$flag = $search_result_array;
-	}
+	// foreach ($keyword_storage as $key => $value) {
+	// 	if(strstr($value['goods_name'], $word) || strstr($value['goods_desc'], $word))
+	// 	{
+	// 		array_push($search_result_array, array('goods_code' => $value['goods_code'],
+	// 												'goods_name' => $value['goods_name']) );
+	// 	}
+	// }
+	// if(count($search_result_array) <= 0) {
+	// 	$flag = "N";
+	// } else {
+	// 	$flag = $search_result_array;
+	// }
 
-	echo json_encode($flag);
+	// echo json_encode($flag);
 ?>
